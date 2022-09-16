63123884abb2870c83815f1fab99a4fbceea48a0
Mike Spaans <99069233@mydavinci.nl>
2022-09-16 14:02:46 +0200

    ssh_connection-GUI.py verbeterd

--- ssh_connection_ GUI.py
+++ ssh_connection_ GUI.py
@@ -1,3 +1,4 @@
+# hier word alles geimporteerd wat nodig is voor dit programma
 import paramiko
 import tkinter as tk
 from functools import partial
@@ -6,11 +7,15 @@ from os.path import exists
 import os
 import time
 
+
+# hier wordt de window aangemaakt
 window = tk.Tk()
 window.geometry("600x400")
 window.title("Tablet Connecter")
 window.configure(background="lightgrey")
 
+
+# ip adress en port van de tablet waarmee verbonden wordt
 host = "192.168.1.100"
 port = 2222
 # username = "Lenovo Tab M10 FHD Plus"
@@ -21,34 +26,51 @@ port = 2222
 def DeleteFile(Path):
     # Bestand in de tablet verwijderen 
     Path = Path.get()
-    # sftp.remove(Path)
-    # storage/emulated/0/Download/testweg.png -- testpad
-    print ("File has been removed")
+    PathCheck = exists(Path)
+    if PathCheck == True:
+        # sftp.remove(Path)
+        # storage/emulated/0/Download/testweg.png -- testpad
+        print ("File has been removed")
+    else:
+        showerror("", "Dit pad of bestand bestaat niet")
 
 def SendFile(Path):
     # bestand vanaf de pi naar de tablet toe sturen
-    # sftp.put("/home/mike/test/testfile.txt", "/storage/emulated/0/Download/testfile.txt") #je moet de naam van het bestand opnieuw typen in de bestemming
-    print ("The file has been transfered to your device")
+    Path = Path.get()
+    PathCheck = exists(Path)
+    if PathCheck == True:
+        # sftp.put("/home/mike/test/testfile.txt", "/storage/emulated/0/Download/testfile.txt") #je moet de naam van het bestand opnieuw typen in de bestemming
+        print ("The file has been transfered to your device")
+    else:
+        showerror("", "Dit pad of bestand bestaat niet")
 
 def ReceiveFile(Path):
     # bestand van de tablet naar de pi sturen
-    # sftp.get("/storage/emulated/0/Download/terugsturen.txt", "/home/mike/test/terugsturen.txt") #je moet de naam van het bestand opnieuw typen in de bestemming
-    print ("the file has been downloaded")
-
+    Path = Path.get()
+    PathCheck = exists(Path)
+    if PathCheck == True:
+        # sftp.get("/storage/emulated/0/Download/terugsturen.txt", "/home/mike/test/terugsturen.txt") #je moet de naam van het bestand opnieuw typen in de bestemming
+        print ("the file has been downloaded")
+    else:
+        showerror("", "Dit pad of bestand bestaat niet")
 
 def AskPath(ChoosenFunction):
+    # Hier worden eerst de tekstbalk knop en tekst verwijderd die niet nodig zijn wanneer je doorklikt
     AskWhatToDo.destroy()
     Textbox.destroy()
     ConnectButton.destroy()
 
+    # Hier word een nieuwe label met tekst aangemaakt om duidelijk te maken dat je het pad in moet vullen
     AskWichPath = tk.Label(window,background="lightgrey", text="Vul het pad in")
     AskWichPath.place(x=200, y=30)
 
     Path = tk.StringVar()
 
+    # Hier wordt de invulbalk aangemaakt waar je het pad in moet voeren
     TextBoxPath = tk.Entry(window, width=30, textvariable=Path)
     TextBoxPath.place(x=200, y=50)
 
+    # knop die doorverwijst naar de functie die de goede actie uitvoert
     SearchPath = tk.Button(text="bestand zoeken", command=partial(ChoosenFunction, Path))
     SearchPath.place(x=400, y=200)
 
@@ -116,7 +138,7 @@ def ConnectDevice(WhatToDo):
                     # sftp.get("/storage/emulated/0/Download/terugsturen.txt", "/home/mike/test/terugsturen.txt") #je moet de naam van het bestand opnieuw typen in de bestemming
                     # print ("the file has been downloaded")
                     AskPath(ReceiveFile)
-
+    # Hier worden een paar errors gegeven als er bepaalde eisen niet kloppen
             except:
                 showerror("","Kan niet verbinden")
         else:
@@ -125,16 +147,18 @@ def ConnectDevice(WhatToDo):
         showerror("", "Je moet een getal invullen")
 
 def Disconnect():
-    # end script
+    # hier wordt de verbinding verbroken met de tablet
     try:
         sftp.close()
 
         ssh.close()
         print ("Verbinding verbroken")
 
+        # Na 2 seconden wordt ook de window gesloten
         time.sleep(2)
         window.destroy()
     except:
+        # als het niet lukt om de verbinding te verbreken dan wordt hier de foutmelding gegeven
         showerror("", "Kan verbinding niet verbreken")
 
 
@@ -142,17 +166,20 @@ def StartWindow():
     global AskWhatToDo
     global Textbox
     global ConnectButton
-    # window aanmaken en de buttons en invoerbalk aanmaken
+    # tekst label met de vraag wat er gedaan moet worden 
     Choise = tk.StringVar()
     AskWhatToDo = tk.Label(window, background="lightgrey", text="Wat wilt u doen? 1). Bestand(en) zoeken \n2). Bestand verwijderen \n3). Bestand Sturen \n4). Bestand Ophalen ")
     AskWhatToDo.place(x=200, y=30)
 
+    # Tekst invoer balk om een nummer aan te geven van wat er gedaan moet worden
     Textbox = tk.Entry(window, width=30, textvariable=Choise)
     Textbox.place(x=200, y=110)
 
+    # Knop om verbinding te maken met de tablet
     ConnectButton = tk.Button(text="Verbind met apparaat", command=partial(ConnectDevice, Choise))
     ConnectButton.place(x=400, y=200)
 
+    # Knop om de verbinding met de tablet te verbreken als dit niet lukt dan krijg je daar een melding van te zien
     DisconnectButton = tk.Button(text="Verbeek verbinding", command=Disconnect)
     DisconnectButton.place(x=100, y=200)
 
@@ -165,7 +192,6 @@ StartWindow()
 
 
 
-# Here you will add all scripts you need to run
 
 
 
